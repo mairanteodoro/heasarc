2aa5aa2db2e8b45afc08d5dab55b27ef8007f3a8
mairanteodoro <mairan.teodoro@gmail.com>
2016-10-19 23:52:48 -0400

    Implement layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app works!';
+
+  list:Array<string> = [
+    'Coordinates conv.', 'Energy conv.',
+    'NH column density', 'Time/Date conv.',
+    'Universal Atomic Database', 'X-ray background',
+    'X-ray/gamma-ray/EUV finder', 'Viewing',
+    'WebPIMMS', 'Latest News',
+    'Upcoming Meetings', 'Proposals',
+    'Skyview'
+  ];
+  listMobile:Array<string> = [
+    'Latest News',
+    'Upcoming Meetings',
+    'Proposal Opportunities',
+    'Tools'
+  ]
+  toolsMobile:Array<string> = [
+    'Coordinates conv.', 'Energy conv.',
+    'NH column density', 'Time/Date conv.',
+    'Universal Atomic Database', 'X-ray background',
+    'X-ray/gamma-ray/EUV finder', 'Viewing',
+    'WebPIMMS', 'Skyview'
+  ];
 
   ngOnInit() {
     $(".dropdown-button").dropdown();
-    $(".button-collapse").sideNav();
+    $('.button-collapse').sideNav({
+      menuWidth: 150, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      }
+    );
     $('.parallax').parallax();
   }
 
